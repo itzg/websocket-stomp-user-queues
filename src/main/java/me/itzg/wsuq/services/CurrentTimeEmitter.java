2ed3e845fbf56de165587284674c308cf227f08a
Geoff Bourne <itzgeoff@gmail.com>
2017-02-06 13:24:12 -0600

    Wait for broker availability

--- src/main/java/me/itzg/wsuq/services/CurrentTimeEmitter.java
+++ src/main/java/me/itzg/wsuq/services/CurrentTimeEmitter.java
@@ -1,17 +1,23 @@
 package me.itzg.wsuq.services;
 
 import me.itzg.wsuq.model.CurrentTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.broker.BrokerAvailabilityEvent;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
 @Service
-public class CurrentTimeEmitter {
+public class CurrentTimeEmitter implements ApplicationListener<BrokerAvailabilityEvent> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CurrentTimeEmitter.class);
 
     private SimpMessagingTemplate messagingTemplate;
+    private boolean brokerAvailable;
 
     @Autowired
     public CurrentTimeEmitter(SimpMessagingTemplate messagingTemplate) {
@@ -20,9 +26,19 @@ public class CurrentTimeEmitter {
 
     @Scheduled(fixedRate = 1000)
     public void emit() {
-        CurrentTime currentTime = new CurrentTime();
-        currentTime.setValue(new Date());
+        if (brokerAvailable) {
+            CurrentTime currentTime = new CurrentTime();
+            currentTime.setValue(new Date());
 
-        messagingTemplate.convertAndSendToUser("user", "/exchange/amq.direct/current-time", currentTime);
+            messagingTemplate.convertAndSendToUser("user", "/exchange/amq.direct/current-time", currentTime);
+        }
+        else {
+            LOGGER.warn("Broker is not yet available");
+        }
+    }
+
+    @Override
+    public void onApplicationEvent(BrokerAvailabilityEvent brokerAvailabilityEvent) {
+        brokerAvailable = brokerAvailabilityEvent.isBrokerAvailable();
     }
 }
